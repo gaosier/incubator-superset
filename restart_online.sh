2f6677bbb5d37ec09f84f18bab2d075f0ae488e7
wushiyong <wushiyong@gaosiedu.com>
2018-03-22 18:21:07 +0800

    1修改数据库，程序启动入口;2新requirement文件生成;3添加wsgi;4上线自动化脚本添加5添加配置文件config_ext及环境变量

---
+++ restart_online.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+vv=$1
+regexp='^[0-9]*(\.[0-9]*)?$'
+if [[ $vv = *[0-9]* && $vv =~ $regexp ]]; then
+    echo "版本号为: $vv"
+else
+    echo "'$vv' 请输入一个合法数字做版本号"
+    exit
+fi
+vv="v${vv}"
+
+cd /data/usr/local/superset&&sed -i "s/\/v[0-9\.]*/\/$vv/g" ./uwsgi.ini&&cd $vv
+
+upid=$(sudo netstat -nlp | grep :15801 | awk '{print $7}' | awk -F"/" '{ print $1 }')
+if [ ! $upid ]; then
+    echo "启动uwsgi"
+    uwsgi --ini /data/usr/local/superset/uwsgi.ini
+else
+    echo "重启uwsgi"
+    uwsgi --reload /data/usr/local/superset/wsgi.pid
+fi
+#sudo kill -9 $upid
+
+pkill -9 -f 'celery worker'
+celery --workdir=/data/usr/local/superset/$vv/superset -A superset.celery  worker -l INFO -D -f /data/usr/local/superset/$vv/superset/celery.log --pidfile=/data/usr/local/superset/$vv/superset/celery.pid
+echo "启动celery"
+
+sudo sed -i "s/\/v[0-9\.]*/\/$vv/g" /etc/nginx/conf.d/python.conf
+sudo service nginx restart
+
---
+++ superset/config_ext.py
@@ -0,0 +1,3 @@
+#-*-coding:utf-8-*-
+import os
+SQLALCHEMY_DATABASE_URI = os.environ['KINGKONG_DB']
