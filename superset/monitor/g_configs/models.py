671284ab39b779c46deb40a9bdc4f384d836c39a
majing <majing@gaosiedu.com>
2018-07-30 15:12:48 +0800

    monitor: add global config

--- superset/monitor/g_configs/models.py
+++ superset/monitor/g_configs/models.py
@@ -20,3 +20,16 @@ class DataOriginConfig(Model, AuditMixinNullable):
     pro_name = Column(String(50), comment=u"项目名称")
     db_name = Column(String(50), comment=u"数据库名称")
     comment = Column(Text, comment=u"备注")
+
+
+class MdConfig(Model, AuditMixinNullable):
+    """
+    埋点数据量配置表
+    """
+    __tablename__ = 'md_config'
+
+    id = Column(Integer, primary_key=True)
+    pro_id = Column(String(20), comment=u"项目ID")
+    name = Column(String(50), comment=u"项目名称")
+    range = Column(String(90), default='[]', comment=u"取值区间")
+    comment = Column(Text, comment=u"备注")
--- superset/monitor/g_configs/views.py
+++ superset/monitor/g_configs/views.py
@@ -3,7 +3,7 @@
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 from superset import appbuilder
-from .models import DataOriginConfig
+from .models import DataOriginConfig, MdConfig
 from ..base import MonitorModelView, DeleteMixin
 from ..base_filters import CommonFilter
 
@@ -41,3 +41,40 @@ appbuilder.add_view(DataOriginConfigModelView, 'OriginConfig',
     category='Global Config',
     category_label='全局配置',
     category_icon='fa-align-left')
+
+
+class MdModelView(MonitorModelView, DeleteMixin):
+    """
+    埋点数据配置页面
+    """
+    datamodel = SQLAInterface(MdConfig)
+
+    list_title = '配置列表'
+    show_title = '详情'
+    add_title = '添加配置'
+    edit_title = '编辑配置'
+
+    search_columns = ('pro_id', 'name')
+    list_columns = ['pro_id','name', 'range', 'creator', 'modified']
+    order_columns = ['pro_id', 'modified']
+    edit_columns = ['pro_id', 'name', 'range', 'comment']
+    add_columns = edit_columns
+    base_order = ('changed_on', 'desc')
+
+    base_filters = [['id', CommonFilter, lambda: []]]
+    label_columns = {
+        'pro_id': '项目ID',
+        'name': '项目名称',
+        'creator': '创建者',
+        'modified': '修改时间',
+        'range': '取值区间',
+        'comment': '备注',
+    }
+
+
+appbuilder.add_view(MdModelView, 'MdConfig',
+                    label='埋点配置',
+                    icon='fa-briefcase',
+                    category='Global Config',
+                    category_label='全局配置',
+                    category_icon='fa-align-left')
