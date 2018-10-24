93989f3d3c2cea4ef9ca45dba4670479a6829067
majing <majing@gaosiedu.com>
2018-10-24 10:43:26 +0800

    add crontab reports emails function

---
+++ superset/tasks/celery_app.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# __author__ = majing
+
+# pylint: disable=C,R,W
+
+"""Utility functions used across Superset"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from superset import app
+from superset.utils import get_celery_app
+
+config = app.config
+app = get_celery_app(config)
