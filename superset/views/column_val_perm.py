52ee9d3a2c896edc81b59930b67b362d7fdba7e3
majing <majing@gaosiedu.com>
2019-04-11 14:44:44 +0800

    [column val perm] modify api

--- superset/views/column_val_perm.py
+++ superset/views/column_val_perm.py
@@ -85,7 +85,7 @@ class ValuePermView(BaseView):
 
         table = ConnectorRegistry.get_datasource('table', pk, db.session)
 
-        data = table.columns if table else []
+        data = [item.verbose_name or item.column_name for item in table.columns] if table else []
         return json_success(json.dumps(data))
 
     @api
